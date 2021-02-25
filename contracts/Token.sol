24e1bd3acfc0531b3abba0212a78d7f53a3a5ff7
davy42 <davy42dev@gmail.com>
2021-02-25 19:28:04 +0300

    Add gitignore and fix path

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
 
